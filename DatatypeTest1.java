cc2eb5bfba22081eb11ce485fa915a34007a988b
praneshsagadev <89292410+praneshsagadev@users.noreply.github.com>
2021-08-21 12:46:45 +0530

    Added datatypeTest1.java

---
+++ DatatypeTest1.java
@@ -0,0 +1,35 @@
+
+package datatypeTest1;
+
+public class DatatypeTest1 {
+	public static void main(String[] args) {
+		boolean ab = true;
+		System.out.println("boolean= " + ab);
+		byte cd = -128;
+		System.out.println(cd);
+		char ef = 'a';
+		System.out.println(ef);
+
+		char ij = '1';
+
+		char gh = 5;
+
+		System.out.println(ij);
+		System.out.println(gh);
+
+//	
+//	short sh= -32768;
+////	
+//	int in=30;
+//	
+//	long  ln= 2147483648l;
+//	
+//	float ff= 214232344554932.3f;
+//	
+//	double df= 2147423232383649.323d;
+//	
+//	System.out.println(ff+" "+df);
+//	 
+
+	}
+}
